83190d4afd17e3e2e7a87aa1996d10623412f504
Anirudh Krishnan Komaralingam <anirudhkrishnanisavailable@gmail.com>
2024-08-12 14:49:45 -0400

    Create example.py

---
+++ src/example.py
@@ -0,0 +1,19 @@
+import math
+
+def multiply(a: float, b: float) -> float:
+    """
+    Multiply two numbers.
+
+    :param a: First number.
+    :param b: Second number.
+    :return: The product of a and b.
+    """
+    return a * b
+
+math.sqrt(4)
+print("hello there")
+if __name__ == "__main__":
+    print("I am the main program")
+
+
+
